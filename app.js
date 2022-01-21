74eece271e5f4a92bed4d20b77cd6f70db8a3a61
Victor <victor@victor.by>
2022-01-22 01:57:03 +0300

    add file

---
+++ app.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide')
+
+// slides по факту массив
+// slides[2].classList.add('active')
+//
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active')
+    })
+}
+
